402b68317ce0f3653c418b15244616b8b36479af
prabudh02 <126944220+prabudh02@users.noreply.github.com>
2025-07-30 09:39:09 +0530

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { switchRole } from '../redux/slices/roleslice';
+import { switchRole } from '../redux/slices/roleSlice';
 import RoleToggle from './RoleToggle';
 import { HiSun, HiMoon } from 'react-icons/hi';
 
